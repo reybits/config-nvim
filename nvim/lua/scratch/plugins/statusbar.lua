e67b447749fa9197f3983326c5374f4b9c8911dd
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-22 11:31:41 +0200

    Different background color for modified buffer.

--- nvim/lua/scratch/plugins/statusbar.lua
+++ nvim/lua/scratch/plugins/statusbar.lua
@@ -6,10 +6,44 @@ return {
     config = function()
         local lualine = require("lualine")
         local lazy_status = require("lazy.status")
+
+        local better_filename = require("lualine.components.filename"):extend()
+        local highlight = require("lualine.highlight")
+
+        function better_filename:init(options)
+            better_filename.super.init(self, options)
+            self.status_colors = {
+                saved = highlight.create_component_highlight_group(
+                    -- with background color
+                    -- { bg = "#228B22" },
+                    -- or without background color
+                    {},
+                    "filename_status_saved",
+                    self.options
+                ),
+                modified = highlight.create_component_highlight_group(
+                    { bg = "#C70039" },
+                    "filename_status_modified",
+                    self.options
+                ),
+            }
+            if self.options.color == nil then
+                self.options.color = ""
+            end
+        end
+
+        function better_filename:update_status()
+            local data = better_filename.super.update_status(self)
+            local color = vim.bo.modified and self.status_colors.modified
+                or self.status_colors.saved
+            data = highlight.component_format_highlight(color) .. data
+            return data
+        end
+
         lualine.setup({
             options = {
                 globalstatus = false,
-                disabled_filetypes = { "NvimTree" },
+                disabled_filetypes = { "NvimTree", "NeogitStatus" },
             },
             -- stylua: ignore
             sections = {
@@ -31,7 +65,8 @@ return {
                         separator = "",
                         padding = { left = 1, right = 0 },
                     },
-                    { "filename" },
+                    -- { "filename" },
+                    { better_filename }
                 },
                 lualine_x = {
                     "diff",

c187abf58bdc722b1fc0c574e18f159165d49a72
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-22 15:04:44 +0200

    Source formatted.

--- nvim/lua/scratch/lazy.lua
+++ nvim/lua/scratch/lazy.lua
@@ -1,23 +1,23 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     { import = "scratch.plugins" },
     { import = "scratch.plugins.lsp" },
-    }, {
+}, {
     checker = {
         enabled = true, -- automatically check for plugin updates
-        frequency = 604800, -- check for updates every week
+        frequency = 86400, -- check for updates every day
         notify = false,
     },
     change_detection = {

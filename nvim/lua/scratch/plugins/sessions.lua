d68a63b3aa53a3324febb5e297645c72e981a758
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-22 23:23:43 +0200

    Dashboard plugin changed.

--- nvim/lua/scratch/plugins/sessions.lua
+++ nvim/lua/scratch/plugins/sessions.lua
@@ -1,4 +1,5 @@
 return {
+    --[[
     {
         "echasnovski/mini.starter",
         version = false,
@@ -17,8 +18,11 @@ return {
                     -- Use this if you set up 'mini.sessions'
                     starter.sections.sessions(3, true),
 
-                    starter.sections.recent_files(5, true),
-                    starter.sections.recent_files(5, false),
+                    -- recent filed (local directory)
+                    starter.sections.recent_files(10, true),
+
+                    -- recent filed (global)
+                    starter.sections.recent_files(30, false),
                 },
                 content_hooks = {
                     starter.gen_hook.adding_bullet(),
@@ -29,10 +33,17 @@ return {
             })
         end,
     },
+    --]]
 
     {
         "echasnovski/mini.sessions",
         version = false,
+        cmd = {
+            "SessionsList",
+            "SessionRead",
+            "SessionReadLast",
+            "SessionWrite",
+        },
         config = function()
             local session = require("mini.sessions")
             session.setup({
@@ -46,12 +57,101 @@ return {
             -- stylua: ignore
             vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
+            vim.api.nvim_create_user_command("SessionsList", function()
+                session.select()
+            end, {
+                desc = "Sessions List",
+            })
+
+            vim.api.nvim_create_user_command("SessionRead", function(args)
+                session.read(args.args)
+            end, {
+                desc = "Read Session",
+            })
+
+            vim.api.nvim_create_user_command("SessionReadLast", function()
+                local last_name = session.get_latest()
+                session.read(last_name)
+            end, {
+                desc = "Read Last Session",
+            })
+
+            vim.api.nvim_create_user_command("SessionWrite", function()
+                session.write()
+            end, {
+                desc = "Write Session",
+            })
+
             vim.keymap.set("n", "<leader>os", function()
                 vim.g.minisessions_disable = not vim.g.minisessions_disable
 
                 -- stylua: ignore
                 print("Session save " .. (vim.g.minisessions_disable and "disabled" or "enabled"))
             end, { desc = "Toggle Store Session" })
+        end,
+    },
+
+    {
+        "nvimdev/dashboard-nvim",
+        event = "VimEnter",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            vim.opt.cursorline = true -- enable cursor line
+
+            require("dashboard").setup({
+                theme = "hyper",
+                config = {
+                    week_header = {
+                        enable = true,
+                    },
+                    disable_move = false,
+                    project = {
+                        enable = false,
+                        limit = 5,
+                        icon = " ",
+                        label = "Browse Files in Directory",
+                        action = "Telescope find_files preview={hide_on_startup=true} cwd=",
+                    },
+                    mru = {
+                        limit = 25,
+                        icon = " ",
+                        label = "Recent Files List",
+                        cwd_only = false,
+                    },
+                    shortcut = {
+                        {
+                            icon = " ", -- "󱉯  "
+                            desc = "Sessions",
+                            group = "Number", --"@property",
+                            action = "SessionsList",
+                            key = "s",
+                        },
+                        {
+                            icon = "󱉯 ", -- "  "
+                            desc = "Last Session",
+                            group = "Number", --"@property",
+                            action = "SessionReadLast",
+                            key = "r",
+                        },
+                        {
+                            icon = "󰉺 ", --   ",
+                            desc = "Files",
+                            group = "Label",
+                            action = "Telescope find_files preview={hide_on_startup=true}",
+                            key = "f",
+                        },
+                        {
+                            icon = " ", -- " 󰊳 ",
+                            desc = "Lazy",
+                            group = "@property",
+                            action = "Lazy",
+                            key = "l",
+                        },
+                    },
+                },
+            })
         end,
     },
 }

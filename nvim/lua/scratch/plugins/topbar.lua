1f39a063cbed2a93b37e508451f766748cdd1224
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-23 12:02:48 +0200

    Use mini.bufremove plugin as close tab function.

--- nvim/lua/scratch/plugins/topbar.lua
+++ nvim/lua/scratch/plugins/topbar.lua
@@ -8,6 +8,10 @@ return {
             options = {
                 mode = "tabs",
                 always_show_bufferline = false,
+                -- stylua: ignore
+                close_command = function(n) require("mini.bufremove").delete(n, false) end,
+                -- stylua: ignore
+                right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
                 color_icons = false,
                 show_buffer_close_icons = false,
                 enforce_regular_tabs = true,

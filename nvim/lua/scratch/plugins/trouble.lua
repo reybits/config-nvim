7e5472d81fde3e8b654a18c302d52be41c110c9b
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-23 23:48:10 +0200

    Trouble events and descriptions updated.

--- nvim/lua/scratch/plugins/trouble.lua
+++ nvim/lua/scratch/plugins/trouble.lua
@@ -1,19 +1,19 @@
 return {
     "folke/trouble.nvim",
-    event = { "BufReadPre", "BufNewFile" },
+    lazy = true,
+    cmd = { "TroubleToggle", "Trouble" },
     dependencies = {
         "nvim-tree/nvim-web-devicons",
-        "folke/todo-comments.nvim",
+        -- "folke/todo-comments.nvim",
     },
     -- stylua: ignore
     keys = {
-        { "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "[TRBL] Toggle" },
-        { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "[TRBL] Workspace Diags" },
-        { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "[TRBL] Document Diags" },
-        { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "[TRBL] Quickfix List" },
-        { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "[TRBL] Location List" },
-        { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "[TRBL] Location List" },
-        { "gr", function() require("trouble").toggle("lsp_references") end, desc = "[TRBL] References List" },
+        { "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Trouble" },
+        { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diags" },
+        { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diags" },
+        { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List" },
+        { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "Location List" },
+        { "gr", "<cmd>lua require('trouble').toggle('lsp_references')<cr>", desc = "References List" },
     },
     opts = {
         use_diagnostic_signs = true,

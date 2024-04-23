334f22266ad2dde009d97a870f94d953914c105b
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-23 14:59:04 +0200

    Loading top and bottom bars at buffer event.

--- nvim/lua/scratch/plugins/topbar.lua
+++ nvim/lua/scratch/plugins/topbar.lua
@@ -1,6 +1,7 @@
 return {
     "akinsho/bufferline.nvim",
     version = "*",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
         local bufferline = require("bufferline")

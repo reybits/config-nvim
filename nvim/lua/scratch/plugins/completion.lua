872e1a8765f4237341b426e257f7d18c2675ee21
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-23 23:45:39 +0200

    Completion fixed.

--- nvim/lua/scratch/plugins/completion.lua
+++ nvim/lua/scratch/plugins/completion.lua
@@ -1,6 +1,6 @@
 return {
     "hrsh7th/nvim-cmp",
-    event = { "BufReadPre", "BufNewFile" },
+    event = { "BufReadPre", "BufNewFile", "CmdlineEnter" },
     dependencies = {
         "neovim/nvim-lspconfig",
         "hrsh7th/cmp-nvim-lsp",
@@ -24,26 +24,33 @@ return {
             },
             snippet = {
                 expand = function(args)
-                    if vim.fn.has("nvim-0.10") then
-                        vim.snippet.expand(args.body) -- native snippets (Neovim v0.10+)
-                    else
+                    if vim.fn.has("nvim-0.9.5") == 1 then
                         vim.fn["vsnip#anonymous"](args.body)
+                    else
+                        vim.snippet.expand(args.body) -- native snippets (Neovim v0.10+)
                     end
                 end,
             },
             mapping = cmp.mapping.preset.insert({
                 ["<c-j>"] = cmp.mapping.select_next_item(),
                 ["<c-k>"] = cmp.mapping.select_prev_item(),
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
                 ["<c-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<c-f>"] = cmp.mapping.scroll_docs(4),
                 ["<c-Space>"] = cmp.mapping.complete(),
-                ["<cr>"] = cmp.mapping.confirm({ select = true }),
+                -- ["<cr>"] = cmp.mapping.confirm({ select = true }),
+                ["<cr>"] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = true,
+                }),
                 ["<c-e>"] = cmp.mapping.abort(),
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
                 { name = "vsnip" },
                 { name = "path" },
+            }, {
                 { name = "buffer" },
             }),
             formatting = {
@@ -72,6 +79,7 @@ return {
         cmp.setup.filetype("gitcommit", {
             sources = cmp.config.sources({
                 { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+            }, {
                 { name = "buffer" },
             }),
         })
@@ -89,6 +97,7 @@ return {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({
                 { name = "path" },
+            }, {
                 { name = "cmdline" },
             }),
             matching = {

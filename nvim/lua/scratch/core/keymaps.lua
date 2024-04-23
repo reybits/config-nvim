efd27532744795d3f41ebe7f9aaa261d1be719aa
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-23 23:44:00 +0200

    Useful hotkeys added.

--- nvim/lua/scratch/core/keymaps.lua
+++ nvim/lua/scratch/core/keymaps.lua
@@ -1,12 +1,28 @@
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 map("n", "<leader>ul", "<cmd>Lazy<cr>", { desc = "Lazy Plugin Manager" })
 map("n", "<leader>um", "<cmd>Mason<cr>", { desc = "Mason Manager" })
 
-map("n", "<esc>", "<cmd>nohl<cr>", { desc = "Clear Search Highlights" })
+map("n", "<esc>", "<cmd>nohl<cr>", opts)
 
 map("n", "<leader>wv", "<c-w>v<cr>", { desc = "Split Window Vertically" })
 map("n", "<leader>wh", "<c-w>s<cr>", { desc = "Split Window Horizontally" })
 
 map("n", "<leader>wd", "<cmd>close<cr>", { desc = "Close Current Window" })
 map("n", "<leader>wD", "<c-w><c-o><cr>", { desc = "Close Other Windows" })
+
+-- Move selected line / block of text in visual mode
+map("v", "J", ":m '>+1<cr>gv=gv", opts)
+map("v", "K", ":m '<-2<cr>gv=gv", opts)
+
+-- paste over currently selected text without yanking it
+map("v", "p", '"_dp')
+map("v", "P", '"_dP')
+
+-- Navigate tabs
+map("n", "<right>", ":tabnext<cr>", opts)
+map("n", "<left>", ":tabprevious<cr>", opts)
+
+map("n", "<up>", "<nop>", opts)
+map("n", "<down>", "<nop>", opts)

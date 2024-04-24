298f43f973671c70b0346fc6e482490adf98150a
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-24 17:28:55 +0200

    Close quickfix and related windows by 'q'.

--- nvim/lua/scratch/core/autocmds.lua
+++ nvim/lua/scratch/core/autocmds.lua
@@ -1,23 +1,55 @@
+--- augroup helper function ----------------------------------------------------
+
+local function augroup(name, opts)
+    opts = opts or { clear = true }
+    return vim.api.nvim_create_augroup("scratch_" .. name, opts)
+end
+
 --- enable cursor line for current buffer only ---------------------------------
+
+local grp_cursorline = augroup("cursorline")
+
 vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
+    group = grp_cursorline,
     pattern = { "*" },
     command = "setlocal cursorline",
 })
 vim.api.nvim_create_autocmd({ "WinLeave" }, {
+    group = grp_cursorline,
     pattern = { "*" },
     command = "setlocal nocursorline",
 })
---[[
---- and disable it for .xml ----------------------------------------------------
-vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
-    pattern = { "*.xml" },
-    command = "setlocal nocursorline",
-})
---]]
 
 --- restore cursor position ----------------------------------------------------
+
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-    desc = "return cursor to where it was last time closing the file",
+    group = augroup("restore_cursor"),
     pattern = { "*" },
     command = 'silent! normal! g`"zv',
+})
+
+--- close some filetypes with <q> ----------------------------------------------
+
+vim.api.nvim_create_autocmd("FileType", {
+    group = augroup("close_with_q"),
+    pattern = {
+        "PlenaryTestPopup",
+        "help",
+        "lspinfo",
+        "notify",
+        "qf",
+        "query",
+        "spectre_panel",
+        "startuptime",
+        "tsplayground",
+        "neotest-output",
+        "checkhealth",
+        "neotest-summary",
+        "neotest-output-panel",
+    },
+    callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        -- stylua: ignore
+        vim.keymap.set( "n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    end,
 })

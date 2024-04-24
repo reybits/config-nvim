3114b5784eb598e84da171ea676783599077a3b9
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-25 00:56:37 +0200

    Treesitter playground added. Indent and highlight disabled.

--- nvim/lua/scratch/plugins/treesitter.lua
+++ nvim/lua/scratch/plugins/treesitter.lua
@@ -3,14 +3,18 @@ return {
         "nvim-treesitter/nvim-treesitter",
         event = { "BufReadPost", "BufNewFile" },
         build = ":TSUpdate",
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+        dependencies = {
+            "nvim-treesitter/playground",
+        },
         config = function()
             local treesitter = require("nvim-treesitter.configs")
             treesitter.setup({
                 highlight = {
-                    enable = true,
+                    enable = false,
                 },
                 indent = {
-                    enable = true,
+                    enable = false,
                     -- disable = { "c", "cpp" },
                 },
                 autotag = {
@@ -29,6 +33,7 @@ return {
                     "css",
                     "html",
                     "javascript",
+                    "java",
                     "json",
                     "lua",
                     "markdown",
@@ -42,6 +47,28 @@ return {
                     "vim",
                     "vimdoc",
                     "yaml",
+                },
+            })
+
+            local treesitter_cfg = require("nvim-treesitter.configs")
+            treesitter_cfg.setup({
+                playground = {
+                    enable = true,
+                    disable = {},
+                    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+                    persist_queries = false, -- Whether the query persists across vim sessions
+                    keybindings = {
+                        toggle_query_editor = "o",
+                        toggle_hl_groups = "i",
+                        toggle_injected_languages = "t",
+                        toggle_anonymous_nodes = "a",
+                        toggle_language_display = "I",
+                        focus_language = "f",
+                        unfocus_language = "F",
+                        update = "R",
+                        goto_node = "<cr>",
+                        show_help = "?",
+                    },
                 },
             })
         end,

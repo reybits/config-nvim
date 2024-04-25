58bb33aa2b5b182fd293d605f668d28a5f6774a6
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-25 12:57:56 +0200

    Aerial navigation plugin added.

---
+++ nvim/lua/scratch/plugins/aerial.lua
@@ -0,0 +1,19 @@
+return {
+    "stevearc/aerial.nvim",
+    event = "VeryLazy",
+    -- stylua: ignore
+    keys = {
+        { "<leader>a", "<cmd>AerialToggle left<cr>", desc = "Show Areal" },
+    },
+    opts = {
+        backends = { "lsp", "treesitter", "markdown", "asciidoc", "man" },
+        layout = {
+            max_width = { 70, 0.4 },
+            default_direction = "prefer_left",
+        },
+    },
+    -- dependencies = {
+    --     "nvim-treesitter/nvim-treesitter",
+    --     "nvim-tree/nvim-web-devicons",
+    -- },
+}

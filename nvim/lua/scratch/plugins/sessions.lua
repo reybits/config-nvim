b0e01caec5fd34599e47ef5c5f7478814e33cb8e
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-28 04:54:04 +0200

    Mini.starter plugin removed.

--- nvim/lua/scratch/plugins/sessions.lua
+++ nvim/lua/scratch/plugins/sessions.lua
@@ -1,40 +1,4 @@
 return {
-    --[[
-    {
-        "echasnovski/mini.starter",
-        version = false,
-        event = "VimEnter",
-        dependencies = {
-            "echasnovski/mini.sessions",
-            version = false,
-        },
-        config = function()
-            local starter = require("mini.starter")
-            starter.setup({
-                evaluate_single = true,
-                -- header = "Hello",
-                silent = true,
-                items = {
-                    -- Use this if you set up 'mini.sessions'
-                    starter.sections.sessions(3, true),
-
-                    -- recent filed (local directory)
-                    starter.sections.recent_files(10, true),
-
-                    -- recent filed (global)
-                    starter.sections.recent_files(30, false),
-                },
-                content_hooks = {
-                    starter.gen_hook.adding_bullet(),
-                    -- starter.gen_hook.indexing("all", { "Builtin actions" }),
-                    -- starter.gen_hook.padding(3, 2),
-                    starter.gen_hook.aligning("center", "center"),
-                },
-            })
-        end,
-    },
-    --]]
-
     {
         "echasnovski/mini.sessions",
         version = false,

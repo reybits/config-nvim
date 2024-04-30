8fb0b671f426007fbf20cc1ff83d07261cd3f6c6
Andrey Ugolnik <andrey@ugolnik.info>
2024-04-30 02:02:38 +0200

    New colorschemes added.

--- nvim/lua/scratch/plugins/colorscheme.lua
+++ nvim/lua/scratch/plugins/colorscheme.lua
@@ -1,13 +1,23 @@
 return {
-    -- "ellisonleao/gruvbox.nvim",
     "EdenEast/nightfox.nvim",
-    -- "catppuccin/nvim", name = "catppuccin",
+    dependencies = {
+        "ellisonleao/gruvbox.nvim",
+        "noahfrederick/vim-noctu",
+        "nickcharlton/vim-materia",
+        { "catppuccin/nvim", name = "catppuccin" },
+
+        "jeffkreeftmeijer/vim-dim", -- ansi colors
+    },
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
         -- load the colorscheme here
-        -- vim.cmd([[colorscheme gruvbox]])
         vim.cmd([[colorscheme duskfox]])
+        -- vim.cmd([[colorscheme gruvbox]])
+        -- vim.cmd([[colorscheme noctu]])
         -- vim.cmd([[colorscheme catppuccin]])
+
+        -- vim.opt.termguicolors = false
+        -- vim.cmd([[colorscheme dim]])
     end,
 }

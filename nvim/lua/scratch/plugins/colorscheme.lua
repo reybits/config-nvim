8102c787130c63ec89a500f5619857e76d227b37
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-05-01 11:10:19 +0200

    Unused colorschemes disabled.

--- nvim/lua/scratch/plugins/colorscheme.lua
+++ nvim/lua/scratch/plugins/colorscheme.lua
@@ -1,12 +1,15 @@
 return {
     "EdenEast/nightfox.nvim",
     dependencies = {
-        "ellisonleao/gruvbox.nvim",
-        "noahfrederick/vim-noctu",
-        "nickcharlton/vim-materia",
-        { "catppuccin/nvim", name = "catppuccin" },
+        -- { "ellisonleao/gruvbox.nvim" },
+        -- { "catppuccin/nvim", name = "catppuccin" },
 
-        "jeffkreeftmeijer/vim-dim", -- ansi colors
+        --  for 16-color terminals
+        -- { "noahfrederick/vim-noctu" },
+        -- { "nickcharlton/vim-materia" },
+
+        -- ansi colors (clone of Vim’s default colorscheme)
+        { "jeffkreeftmeijer/vim-dim" },
     },
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
@@ -14,10 +17,11 @@ return {
         -- load the colorscheme here
         vim.cmd([[colorscheme duskfox]])
         -- vim.cmd([[colorscheme gruvbox]])
-        -- vim.cmd([[colorscheme noctu]])
         -- vim.cmd([[colorscheme catppuccin]])
 
         -- vim.opt.termguicolors = false
+        -- vim.cmd([[colorscheme noctu]])
+        -- vim.cmd([[colorscheme interrobang]])
         -- vim.cmd([[colorscheme dim]])
     end,
 }

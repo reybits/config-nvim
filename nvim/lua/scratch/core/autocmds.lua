9d04b78ba3b93b851cc4b91915aab58c7a7d2f13
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-05-12 13:10:47 +0200

    The git file type has been added to the list of windows that close with "q".

--- nvim/lua/scratch/core/autocmds.lua
+++ nvim/lua/scratch/core/autocmds.lua
@@ -46,6 +46,7 @@ vim.api.nvim_create_autocmd("FileType", {
         "checkhealth",
         "neotest-summary",
         "neotest-output-panel",
+        "git",
     },
     callback = function(event)
         vim.bo[event.buf].buflisted = false

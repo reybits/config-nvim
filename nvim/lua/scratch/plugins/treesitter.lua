e0a020c212407d5409f38fcf7f6654073053305b
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-05-18 04:00:51 +0200

    feat(treesitter): playground removed.
    The nvim-treesitter/playground plugin has been removed in favor
    of nvim-0.10.

--- nvim/lua/scratch/plugins/treesitter.lua
+++ nvim/lua/scratch/plugins/treesitter.lua
@@ -4,9 +4,6 @@ return {
         event = { "BufReadPost", "BufNewFile" },
         build = ":TSUpdate",
         cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-        dependencies = {
-            "nvim-treesitter/playground",
-        },
         config = function()
             local treesitter = require("nvim-treesitter.configs")
             treesitter.setup({
@@ -47,28 +44,6 @@ return {
                     "vim",
                     "vimdoc",
                     "yaml",
-                },
-            })
-
-            local treesitter_cfg = require("nvim-treesitter.configs")
-            treesitter_cfg.setup({
-                playground = {
-                    enable = true,
-                    disable = {},
-                    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-                    persist_queries = false, -- Whether the query persists across vim sessions
-                    keybindings = {
-                        toggle_query_editor = "o",
-                        toggle_hl_groups = "i",
-                        toggle_injected_languages = "t",
-                        toggle_anonymous_nodes = "a",
-                        toggle_language_display = "I",
-                        focus_language = "f",
-                        unfocus_language = "F",
-                        update = "R",
-                        goto_node = "<cr>",
-                        show_help = "?",
-                    },
                 },
             })
         end,

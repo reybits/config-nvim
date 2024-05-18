5bf678d120d818f090b2884183f92288578ce885
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-05-18 02:55:59 +0200

    feat(git): fugitive instead of neogit.

--- nvim/lua/scratch/plugins/gitsupport.lua
+++ nvim/lua/scratch/plugins/gitsupport.lua
@@ -1,14 +1,38 @@
 return {
     -- best git wrapper for vim
-    --[[
-    "tpope/vim-fugitive",
-    -- stylua: ignore
-    keys = {
-        { "<leader>gg", "<cmd>Git<cr>", desc = "Git Fugitive" },
-        { "<leader>gb", "<cmd>Git blame<cr>", desc = "Toggle Git Blame" },
+    {
+        "tpope/vim-fugitive",
+        -- stylua: ignore
+        cmd = {
+            "G", "Git",
+            "GBrowse", "GDelete", "GRemove", "GUnlink", "GMove", "GRename",
+            "Gcd", "Glcd", "Gclog", "Gllog", "Gdiffsplit", "Gdrop", "Gedit",
+            "Ggrep", "Glgrep", "Gpedit", "Gread", "Gtabedit", "Gsplit",
+            "Gvdiffsplit", "Gvsplit", "Gwq", "Gwrite"
+        },
+        keys = {
+            { "<leader>gg", "<cmd>Git<cr>", desc = "Git Fugitive" },
+            { "<leader>gb", "<cmd>Git blame<cr>", desc = "Toggle Git Blame" },
+        },
+        init = function()
+            -- disables legacy commands (like as Gbrowse, Gremove, Grename).
+            vim.g.fugitive_legacy_commands = 0
+        end,
     },
-    --]]
 
+    -- tig is no longer needed :)
+    {
+        "rbong/vim-flog",
+        cmd = { "Flog", "Flogsplit", "Floggit" },
+        keys = {
+            { "<leader>gl", "<cmd>Flog<cr>", desc = "A git branch viewer" },
+        },
+        dependencies = {
+            "tpope/vim-fugitive",
+        },
+    },
+
+    --[[
     -- clone of magit
     "NeogitOrg/neogit",
     event = { "BufReadPre", "BufNewFile" },
@@ -26,4 +50,5 @@ return {
         { "<leader>gG", "<cmd>Neogit cwd=%:p:h<cr>", desc = "Neogit (cwd)" },
         { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit (project root)" },
     },
+    --]]
 }

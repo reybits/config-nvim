1eb1dbfc75b58873a53f7584cd4458ae268bf177
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-05-18 11:52:33 +0200

    fix(telescope): yet another buffers sort option.

--- nvim/lua/scratch/plugins/telescope.lua
+++ nvim/lua/scratch/plugins/telescope.lua
@@ -132,8 +132,8 @@ return {
             pickers = {
                 buffers = {
                     ignore_current_buffer = true,
-                    sort_lastused = true,
-                    -- sort_mru = true,
+                    -- sort_lastused = true,
+                    sort_mru = true,
                     mappings = {
                         i = {
                             ["<c-d>"] = actions.delete_buffer,

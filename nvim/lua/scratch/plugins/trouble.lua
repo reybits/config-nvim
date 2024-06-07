95a0edfd0efbca1f3724bc1c562612576fb6be59
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-06-07 12:42:08 +0400

    fix(trouble): hotkeys fixed.

--- nvim/lua/scratch/plugins/trouble.lua
+++ nvim/lua/scratch/plugins/trouble.lua
@@ -9,13 +9,13 @@ return {
     -- stylua: ignore
     keys = {
         -- { "<leader>xx", "<cmd>Trouble<cr>", desc = "Trouble" },
-        { "<leader>xw", "<cmd>Trouble lsp toggle<cr>", desc = "Full LSP" },
-        { "<leader>xw", "<cmd>Trouble lsp_document_symbols toggle<cr>", desc = "Symbols" },
-        { "<leader>xw", "<cmd>Trouble lsp_incoming_calls toggle<cr>", desc = "Incoming Calls" },
-        { "<leader>xw", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics" },
+        { "<leader>xx", "<cmd>rouble lsp toggle<cr>", desc = "Full LSP" },
+        { "<leader>xs", "<cmd>Trouble lsp_document_symbols toggle<cr>", desc = "Symbols" },
+        { "<leader>xi", "<cmd>Trouble lsp_incoming_calls toggle<cr>", desc = "Incoming Calls" },
+        { "<leader>xd", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics" },
         { "<leader>xq", "<cmd>Trouble quickfix toggle<cr>", desc = "Quickfix List" },
         { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List" },
-        { "<leader>xw", "<cmd>Trouble todo toggle<cr>", desc = "Todo" },
+        { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo" },
         { "gr", "<cmd>Trouble lsp_references toggle<cr>", desc = "References List" },
     },
     opts = {

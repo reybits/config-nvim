3470e09186abaa7401356ccf1d44dcf9e9505082
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-09-09 11:07:31 +0200

    fix(trouble)!: update mappings

--- nvim/lua/scratch/plugins/trouble.lua
+++ nvim/lua/scratch/plugins/trouble.lua
@@ -6,17 +6,55 @@ return {
         "nvim-tree/nvim-web-devicons",
         -- "folke/todo-comments.nvim",
     },
-    -- stylua: ignore
     keys = {
-        -- { "<leader>xx", "<cmd>Trouble<cr>", desc = "Trouble" },
-        { "<leader>xx", "<cmd>Trouble lsp toggle<cr>", desc = "Full LSP" },
-        { "<leader>xs", "<cmd>Trouble lsp_document_symbols toggle<cr>", desc = "Symbols" },
-        { "<leader>xi", "<cmd>Trouble lsp_incoming_calls toggle<cr>", desc = "Incoming Calls" },
-        { "<leader>xd", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics" },
-        { "<leader>xq", "<cmd>Trouble quickfix toggle<cr>", desc = "Quickfix List" },
-        { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List" },
-        { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo" },
-        { "gr", "<cmd>Trouble lsp_references toggle<cr>", desc = "References List" },
+        {
+            "<leader>xx",
+            -- "<cmd>Trouble lsp toggle<cr>",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP related (Trouble)",
+        },
+        {
+            "<leader>xd",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xD",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xq",
+            -- "<cmd>Trouble quickfix toggle<cr>",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
+        {
+            "<leader>xl",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xt",
+            "<cmd>Trouble todo toggle<cr>",
+            desc = "Todo (Trouble)",
+        },
+        {
+            "<leader>xs",
+            -- "<cmd>Trouble lsp_document_symbols toggle<cr>",
+            "<cmd>Trouble symbols toggle focus=true win.position=left<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>xr",
+            "<cmd>Trouble lsp_references toggle<cr>",
+            desc = "References List (Trouble)",
+        },
+        {
+            "<leader>xi",
+            "<cmd>Trouble lsp_incoming_calls toggle<cr>",
+            desc = "Incoming Calls (Trouble)",
+        },
     },
     opts = {
         use_diagnostic_signs = true,

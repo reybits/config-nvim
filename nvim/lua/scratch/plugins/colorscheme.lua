460b03ef2b0751bd7d6e22ce7ca25ef9ba6aaa62
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-02-18 12:28:32 +0100

    fix(cursor): disable insert mode

--- nvim/lua/scratch/plugins/colorscheme.lua
+++ nvim/lua/scratch/plugins/colorscheme.lua
@@ -54,6 +54,8 @@ return {
     {
         "sphamba/smear-cursor.nvim",
         opts = {
+            smear_insert_mode = false,
+
             stiffness = 0.8, -- 0.6 [0, 1]
             trailing_stiffness = 0.5, -- 0.3 [0, 1]
             distance_stop_animating = 0.5, -- 0.1 [>0]

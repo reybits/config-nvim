cb5ab28f6258d3ad6778c52a7209558b61f85e36
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-02-19 19:34:09 +0100

    feat(cursor): remove sphamba/smear-cursor.nvim plugin

--- nvim/lua/scratch/plugins/colorscheme.lua
+++ nvim/lua/scratch/plugins/colorscheme.lua
@@ -50,15 +50,4 @@ return {
         "nickcharlton/vim-materia",
         lazy = true,
     },
-
-    {
-        "sphamba/smear-cursor.nvim",
-        opts = {
-            smear_insert_mode = false,
-
-            stiffness = 0.8, -- 0.6 [0, 1]
-            trailing_stiffness = 0.5, -- 0.3 [0, 1]
-            distance_stop_animating = 0.5, -- 0.1 [>0]
-        },
-    },
 }

124861a74b6b2ca423ae8fd4df0be6c780780403
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-02-22 12:45:22 +0100

    feat(harpoon): navigate with number keys, highlight current file

--- nvim/lua/scratch/plugins/harpoon.lua
+++ nvim/lua/scratch/plugins/harpoon.lua
@@ -8,6 +8,14 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
+        local extensions = require("harpoon.extensions")
+
+        -- Highlight current file in the harpoon buffer list
+        harpoon:extend(extensions.builtins.highlight_current_file())
+
+        -- Goto buffer by number keys
+        harpoon:extend(extensions.builtins.navigate_with_number())
+
         vim.keymap.set("n", "<leader>h", function()
             harpoon.ui:toggle_quick_menu(harpoon:list())
         end, { desc = "Open Harpoon window" })
@@ -16,16 +24,6 @@ return {
             vim.notify("File '" .. vim.fn.expand("%") .. "' added to Harpoon", vim.log.levels.INFO)
             harpoon:list():add()
         end, { desc = "Add file to Harpoon" })
-
-        -- vim.keymap.set("n", "1", function() harpoon:list():select(1) end)
-        -- vim.keymap.set("n", "2", function() harpoon:list():select(2) end)
-        -- vim.keymap.set("n", "3", function() harpoon:list():select(3) end)
-        -- vim.keymap.set("n", "4", function() harpoon:list():select(4) end)
-        -- vim.keymap.set("n", "5", function() harpoon:list():select(5) end)
-        -- vim.keymap.set("n", "6", function() harpoon:list():select(6) end)
-        -- vim.keymap.set("n", "7", function() harpoon:list():select(7) end)
-        -- vim.keymap.set("n", "8", function() harpoon:list():select(8) end)
-        -- vim.keymap.set("n", "9", function() harpoon:list():select(9) end)
 
         -- Toggle previous & next buffers stored within Harpoon list
         -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)

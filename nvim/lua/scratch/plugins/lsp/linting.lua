29626182f2364f0bb6bcdd4a56d519f373cb9b5a
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-02-22 14:44:27 +0100

    feat(linter): load on demaind, add LinterHide command

--- nvim/lua/scratch/plugins/lsp/linting.lua
+++ nvim/lua/scratch/plugins/lsp/linting.lua
@@ -1,30 +1,33 @@
 return {
     "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
+    cmd = {
+        "LinterHide",
+    },
+    keys = {
+        {
+            mode = "n",
+            "<leader>cl",
+            function()
+                require("lint").try_lint()
+            end,
+            desc = "Run Code Linter",
+        },
+    },
     config = function()
         local lint = require("lint")
         lint.linters_by_ft = {
             -- c = { "cpplint" },
             -- cpp = { "cpplint" },
             bash = { "shellcheck" },
-            -- lua = { "luacheck" },
+            lua = { "luacheck" },
             javascript = { "quick_lint_js" },
             typecript = { "quick_lint_js" },
         }
 
-        -- local lint_group = vim.api.nvim_create_augroup("lint", { clear = true })
-        -- vim.api.nvim_create_autocmd(
-        --     { "BufEnter", "BufWritePost", "InsertLeave" },
-        --     {
-        --         group = lint_group,
-        --         callback = function()
-        --             lint.try_lint()
-        --         end,
-        --     }
-        -- )
-
-        vim.keymap.set("n", "<leader>cl", function()
-            lint.try_lint()
-        end, { desc = "Run Code Linter" })
+        vim.api.nvim_create_user_command("LinterHide", function()
+            vim.diagnostic.reset(nil, 0)
+        end, {
+            desc = "Hide Linger Diagnostics",
+        })
     end,
 }

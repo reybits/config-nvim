e6b51eb0a7cf185c9f7939e15ebcb02628814447
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-02-25 11:45:26 +0100

    fix(nvim-cmp): remove redundant dependency, activate by InsertEnter event

--- nvim/lua/scratch/plugins/completion.lua
+++ nvim/lua/scratch/plugins/completion.lua
@@ -1,13 +1,15 @@
 return {
     "hrsh7th/nvim-cmp",
-    event = { "BufReadPre", "BufNewFile", "CmdlineEnter" },
+    event = {
+        "InsertEnter",
+        "CmdlineEnter",
+    },
     dependencies = {
         "neovim/nvim-lspconfig",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
         "vurentjie/cmp-gl",
 
         "hrsh7th/cmp-vsnip",

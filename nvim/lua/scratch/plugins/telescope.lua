13392438baf42b9be1fd3167e54d996c7761113c
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-03-04 14:57:24 +0100

    feat(telescope): update sort options

--- nvim/lua/scratch/plugins/telescope.lua
+++ nvim/lua/scratch/plugins/telescope.lua
@@ -190,7 +190,7 @@ return {
             pickers = {
                 buffers = {
                     ignore_current_buffer = true,
-                    -- sort_lastused = true,
+                    sort_lastused = true,
                     sort_mru = true,
                     mappings = {
                         i = {

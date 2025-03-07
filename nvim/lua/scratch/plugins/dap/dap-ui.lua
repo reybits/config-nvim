f136d8fa5dd94b255cb314235cdab5fc766a0d03
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-03-07 11:55:49 +0100

    fix!: add nvim-dap-virtual-text as a dependency of nvim-dap-ui instead of a standalone plugin

--- nvim/lua/scratch/plugins/dap/dap-ui.lua
+++ nvim/lua/scratch/plugins/dap/dap-ui.lua
@@ -1,37 +1,30 @@
 return {
     -- fancy UI for the debugger
-    {
-        "rcarriga/nvim-dap-ui",
-        dependencies = {
-            "mfussenegger/nvim-dap",
-            "nvim-neotest/nvim-nio",
-        },
-        -- stylua: ignore
-        keys = {
-            { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
-            { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
-        },
-        config = function(_, opts)
-            local dapui = require("dapui")
-            dapui.setup(opts)
-
-            local dap = require("dap")
-
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open({})
-            end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close({})
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close({})
-            end
-        end,
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+        "mfussenegger/nvim-dap",
+        "nvim-neotest/nvim-nio",
+        "theHamsta/nvim-dap-virtual-text", -- virtual text for the debugger
     },
-
-    -- virtual text for the debugger
-    {
-        "theHamsta/nvim-dap-virtual-text",
-        opts = {},
+    -- stylua: ignore
+    keys = {
+        { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+        { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
     },
+    config = function(_, opts)
+        local dapui = require("dapui")
+        dapui.setup(opts)
+
+        local dap = require("dap")
+
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open({})
+        end
+        dap.listeners.before.event_terminated["dapui_config"] = function()
+            dapui.close({})
+        end
+        dap.listeners.before.event_exited["dapui_config"] = function()
+            dapui.close({})
+        end
+    end,
 }

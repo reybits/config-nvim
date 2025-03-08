770e5f573e0ce1b76f211aab7233c7d68d3557b2
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-03-08 19:29:42 +0100

    feat(git): add neogit log mapping

--- nvim/lua/scratch/plugins/vcs/neogit.lua
+++ nvim/lua/scratch/plugins/vcs/neogit.lua
@@ -10,13 +10,13 @@ return {
         -- "sindrets/diffview.nvim", -- optional (personally I use gitsigns instead)
         -- "nvim-telescope/telescope.nvim", -- optional (personally I don't use it in this case)
     },
-    config = true,
     opts = {
         graph_style = "unicode",
     },
     -- stylua: ignore
     keys = {
         { "<leader>gG", "<cmd>Neogit cwd=%:p:h<cr>", desc = "Neogit (cwd)" },
-        { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit (project root)" },
+        { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit (project)" },
+        { "<leader>gl", "<cmd>Neogit log<cr>", desc = "Neogit Log (project)" },
     },
 }

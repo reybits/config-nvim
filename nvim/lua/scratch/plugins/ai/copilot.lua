d0bdc1c66500d8764a12a2c4ce8455eee9b8ebd0
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-03-09 13:49:25 +0100

    fix(copilot): stop/resume copilot when Typr is active/inactive

--- nvim/lua/scratch/plugins/ai/copilot.lua
+++ nvim/lua/scratch/plugins/ai/copilot.lua
@@ -7,4 +7,30 @@ return {
     event = {
         "InsertEnter",
     },
+    init = function()
+        -- This functions intended to be used to stop/resume copilot when Typr is active
+        vim.api.nvim_create_autocmd("InsertEnter", {
+            pattern = "*",
+            callback = function()
+                -- print("Type: " .. vim.bo.filetype)
+                if vim.bo.filetype == "typr" then
+                    -- print(vim.inspect(args))
+                    vim.notify("Copilot disabled", vim.log.levels.INFO)
+                    vim.cmd("Copilot disable")
+                end
+            end,
+        })
+
+        vim.api.nvim_create_autocmd("InsertLeave", {
+            pattern = "*",
+            callback = function()
+                -- print("Type: " .. vim.bo.filetype)
+                if vim.bo.filetype == "typr" then
+                    -- print(vim.inspect(args))
+                    vim.notify("Copilot enabled", vim.log.levels.INFO)
+                    vim.cmd("Copilot enable")
+                end
+            end,
+        })
+    end,
 }

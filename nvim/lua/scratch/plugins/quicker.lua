38c1c18b7169bfd868089b08b7629597594c8544
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-03-09 15:01:36 +0100

    fix(quicker)!: change loclist key mapping

--- nvim/lua/scratch/plugins/quicker.lua
+++ nvim/lua/scratch/plugins/quicker.lua
@@ -6,7 +6,7 @@ return {
     -- stylua: ignore
     keys = {
         { "<leader>q", function() require("quicker").toggle() end, desc = "Toggle quickfix" },
-        { "<leader>l", function() require("quicker").toggle({ loclist = true }) end, desc = "Toggle loclist" },
+        { "<leader>Q", function() require("quicker").toggle({ loclist = true }) end, desc = "Toggle loclist" },
     },
     config = function()
         require("quicker").setup({

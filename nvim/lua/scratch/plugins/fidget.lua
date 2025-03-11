5e01c9a07b2ae80c129e905e7660d362d211b2e7
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-03-11 18:13:12 +0100

    feat(fidget): truncate long messages (enabled by default)

--- nvim/lua/scratch/plugins/fidget.lua
+++ nvim/lua/scratch/plugins/fidget.lua
@@ -1,14 +1,37 @@
+--
+-- Extensible UI for Neovim notifications and LSP progress messages.
+--
+
+local ToggleOption = require("scratch.core.toggleopt")
+
+local toggle_truncte = ToggleOption:new("<leader>ot", nil, "Truncate Notifications", true)
+
 return {
-    -- Extensible UI for Neovim notifications and LSP progress messages.
     "j-hui/fidget.nvim",
     event = "VeryLazy",
+    keys = {
+        {
+            toggle_truncte:getMapping(),
+            toggle_truncte:getToggleFunc(),
+            desc = toggle_truncte:getCurrentDescription(),
+        },
+    },
     opts = {
         notification = {
-            -- Automatically override vim.notify() with Fidget
-            override_vim_notify = true,
+            override_vim_notify = true, -- override vim.notify() with Fidget
+
             view = {
-                stack_upwards = false, -- if true, set align to 'bottom'
+                stack_upwards = false, -- if true, set `notification.window.align` to "bottom"
+                render_message = function(msg, cnt)
+                    local message = cnt == 1 and msg or string.format("(%dx) %s", cnt, msg)
+                    if toggle_truncte:getState() then
+                        local width = math.floor(vim.api.nvim_win_get_width(0) / 3)
+                        return require("scratch.core.helpers").truncate(message, width)
+                    end
+                    return message
+                end,
             },
+
             window = {
                 winblend = 0,
                 zindex = 9999,

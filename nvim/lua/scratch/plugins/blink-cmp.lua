5c14810352f4b602499fea3230ef8da522821da4
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-03-12 13:51:07 +0100

    fix(blink-cmp): disable completion in DressingInput

--- nvim/lua/scratch/plugins/blink-cmp.lua
+++ nvim/lua/scratch/plugins/blink-cmp.lua
@@ -24,7 +24,7 @@ return {
     opts = {
         -- Disable blink-cmp for some filetype
         enabled = function()
-            return not vim.tbl_contains({ "typr" }, vim.bo.filetype)
+            return not vim.tbl_contains({ "typr", "DressingInput" }, vim.bo.filetype)
                 and vim.bo.buftype ~= "prompt"
                 and vim.b.completion ~= false
         end,

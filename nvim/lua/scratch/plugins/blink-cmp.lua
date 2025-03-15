7d884865c5283b2d0dffab6874a6bd02e38030c7
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-03-15 17:57:54 +0100

    refactor: move lazydev to a separate config file
    Moved the lazydev plugin configuration from blink-cmp.lua to a new
    file lazydev.lua for better modularity and maintainability.

--- nvim/lua/scratch/plugins/blink-cmp.lua
+++ nvim/lua/scratch/plugins/blink-cmp.lua
@@ -13,17 +13,7 @@ return {
 
         "xzbdmw/colorful-menu.nvim",
 
-        {
-            "folke/lazydev.nvim",
-            ft = "lua", -- only load on lua files
-            opts = {
-                library = {
-                    -- See the configuration section for more details
-                    -- Load luvit types when the `vim.uv` word is found
-                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                },
-            },
-        },
+        "folke/lazydev.nvim",
     },
 
     event = {

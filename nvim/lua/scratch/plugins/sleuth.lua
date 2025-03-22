d07b5d67f280ddf30a98ff09f25997fc013ad3e4
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-03-22 16:08:34 +0100

    feat(plugins): replace vim-sleuth with guess-indent
    Replaced the "tpope/vim-sleuth" plugin with "nmac427/guess-indent.nvim"
    for better heuristic buffer option setting. The new plugin is triggered
    on InsertEnter and provides a GuessIndent command.

--- nvim/lua/scratch/plugins/sleuth.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- Heuristically set buffer options
-    "tpope/vim-sleuth",
-    event = {
-        "InsertEnter",
-    },
-    -- no need to set any options, because the it's a vim plugin
-}

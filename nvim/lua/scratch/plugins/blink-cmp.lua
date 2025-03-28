81f7edca0cadbc3c24fc5ff19b5c5d667ef40d0c
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-03-28 18:37:57 +0100

    feat(plugins): update blink-cmp to version 1.*
    Updated the `blink-cmp` plugin to use version `1.*` instead of `*`. This
    ensures compatibility with the latest stable release while avoiding
    potential issues with future breaking changes.

--- nvim/lua/scratch/plugins/blink-cmp.lua
+++ nvim/lua/scratch/plugins/blink-cmp.lua
@@ -1,7 +1,7 @@
 return {
     "saghen/blink.cmp",
     -- use a release tag to download pre-built binaries
-    version = "*",
+    version = "1.*",
     -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
     -- build = 'cargo build --release',
     -- If you use nix, you can build from source using latest nightly rust with:

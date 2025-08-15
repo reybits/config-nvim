f00a44df328b14d9c72e2574977dfd6b19ab8d07
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-08-16 01:05:16 +0200

    fix(copilot): disable event trigger due to fzf-lua issue
    Commented out Copilot event trigger to avoid errors with fzf-lua as described
    in issue #2247. Reference: https://github.com/ibhagwan/fzf-lua/issues/2247

--- nvim/lua/scratch/plugins/ai/copilot.lua
+++ nvim/lua/scratch/plugins/ai/copilot.lua
@@ -6,12 +6,16 @@ return {
     cmd = {
         "Copilot",
     },
+    --[[
+    -- Disabled due error described in the issue below.
+    -- https://github.com/ibhagwan/fzf-lua/issues/2247#issuecomment-3192963942
     event = {
         -- Only "BufWinEnter" is required for Copilot to work when using the blink-cmp plugin.
         -- Related to blink-copilot documentation: https://github.com/fang2hou/blink-copilot
         "BufWinEnter",
         -- "InsertEnter",
     },
+    --]]
     init = function()
         -- Related to blink-copilot documentation: https://github.com/fang2hou/blink-copilot
         vim.g.copilot_no_maps = true

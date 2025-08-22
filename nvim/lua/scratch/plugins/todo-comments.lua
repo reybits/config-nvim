024c8dbdc46634ab72bea20406a4e62c8bb4c6d7
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-08-22 20:15:09 +0200

    feat(todo-comments): add TodoLocList and TodoQuickFix commands

--- nvim/lua/scratch/plugins/todo-comments.lua
+++ nvim/lua/scratch/plugins/todo-comments.lua
@@ -1,3 +1,25 @@
+--------------------------------------------------------------------------------
+-- Plugin to highlight and search for todo comments:
+-- TODO: todo text
+-- FIXME: fixme text
+-- FIXIT: fixit text
+-- BUG: bug text
+-- HACK: hack text
+-- WARN: some warnig
+-- XXX: some exclamation
+-- WARNING: some warnig
+-- PERF: performance info
+-- OPTIM: optimization info
+-- PERFORMANCE: performance info
+-- OPTIMIZE: optimize info
+-- NOTE: note text
+-- INFO: some info
+-- TEST: test text
+-- TESTING: testing info
+-- PASSED: passed info
+-- FAILED: falied info
+--------------------------------------------------------------------------------
+
 return {
     "folke/todo-comments.nvim",
     event = {
@@ -9,22 +31,13 @@ return {
     },
     cmd = {
         "TodoFzfLua",
+        "TodoLocList",
+        "TodoQuickFix",
     },
+    -- stylua: ignore
     keys = {
-        {
-            "]t",
-            function()
-                require("todo-comments").jump_next()
-            end,
-            desc = "Next TODO comment",
-        },
-        {
-            "[t",
-            function()
-                require("todo-comments").jump_prev()
-            end,
-            desc = "Prev TODO comment",
-        },
+        { "]t", function() require("todo-comments").jump_next() end, desc = "Next TODO comment", },
+        { "[t", function() require("todo-comments").jump_prev() end, desc = "Prev TODO comment", },
     },
     opts = {},
 }

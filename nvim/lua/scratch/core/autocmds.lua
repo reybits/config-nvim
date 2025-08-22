133484a0eb23283b8954e6b8af007223eb58b58a
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-08-22 20:17:17 +0200

    fix(autocmds): guard buffer actions with validity check
    Add a check for buffer validity before setting 'buflisted' and keymaps in
    autocmd callback. Prevents errors when the buffer is invalid.

--- nvim/lua/scratch/core/autocmds.lua
+++ nvim/lua/scratch/core/autocmds.lua
@@ -50,9 +50,10 @@ vim.api.nvim_create_autocmd("FileType", {
         "tsplayground",
     },
     callback = function(event)
-        vim.bo[event.buf].buflisted = false
-        -- stylua: ignore
-        vim.keymap.set( "n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+        if vim.api.nvim_buf_is_valid(event.buf) then
+            vim.bo[event.buf].buflisted = false
+            vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+        end
     end,
 })
 

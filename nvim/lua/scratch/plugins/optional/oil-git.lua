9bd1dc90c8c382280e5fc0b79a6cb1c794ca245c
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-10-18 17:43:37 +0200

    feat(plugins): add oil-git.nvim integration
    Added oil-git.nvim plugin with oil.nvim as a dependency. No additional
    configuration required; works automatically upon installation.

---
+++ nvim/lua/scratch/plugins/optional/oil-git.lua
@@ -0,0 +1,9 @@
+return {
+    "benomahony/oil-git.nvim",
+    dependencies = {
+        "stevearc/oil.nvim",
+    },
+    opts = {
+        -- No opts or config needed! Works automatically
+    },
+}

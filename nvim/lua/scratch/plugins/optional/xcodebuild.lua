aa99676098594da2e1b0f5c040333c294696ef1a
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-10-31 22:40:50 +0100

    feat(xcodebuild): update keymaps and group name
    - Refactor Xcodebuild plugin keymaps for better organization and clarity.
    - Add new keybindings for project manager and device selection.
    - Update group name in which-key from "Xcode Build" to "Xcode".
    - Add and comment out additional keymaps for future features and actions.

--- nvim/lua/scratch/plugins/optional/xcodebuild.lua
+++ nvim/lua/scratch/plugins/optional/xcodebuild.lua
@@ -15,11 +15,32 @@ return {
         -- "nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
     },
     keys = {
-        { "<leader>xp", "<cmd>XcodebuildPicker<cr>", desc = "Open Picker" },
-        { "<leader>xb", "<cmd>XcodebuildBuild<cr>", desc = "Build" },
-        { "<leader>xr", "<cmd>XcodebuildBuildRun<cr>", desc = "Build and Run" },
-        { "<leader>xd", "<cmd>XcodebuildBuildDebug<cr>", desc = "Build and Debug" },
-        { "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", desc = "Toggle Logs" },
+        { "<leader>X", "<cmd>XcodebuildPicker<cr>", desc = "Show Xcodebuild Actions" },
+        { "<leader>xf", "<cmd>XcodebuildProjectManager<cr>", desc = "Show Project Manager Actions" },
+
+        { "<leader>xb", "<cmd>XcodebuildBuild<cr>", desc = "Build Project" },
+        { "<leader>xB", "<cmd>XcodebuildBuildForTesting<cr>", desc = "Build For Testing" },
+        { "<leader>xr", "<cmd>XcodebuildBuildRun<cr>", desc = "Build & Run Project" },
+
+        -- { "<leader>xt", "<cmd>XcodebuildTest<cr>", desc = "Run Tests" },
+        -- { "<leader>xt", "<cmd>XcodebuildTestSelected<cr>", desc = "Run Selected Tests" },
+        -- { "<leader>xT", "<cmd>XcodebuildTestClass<cr>", desc = "Run Current Test Class" },
+        -- { "<leader>x.", "<cmd>XcodebuildTestRepeat<cr>", desc = "Repeat Last Test Run" },
+
+        { "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", desc = "Toggle Xcodebuild Logs" },
+        -- { "<leader>xc", "<cmd>XcodebuildToggleCodeCoverage<cr>", desc = "Toggle Code Coverage" },
+        -- { "<leader>xC", "<cmd>XcodebuildShowCodeCoverageReport<cr>", desc = "Show Code Coverage Report" },
+        -- { "<leader>xe", "<cmd>XcodebuildTestExplorerToggle<cr>", desc = "Toggle Test Explorer" },
+        -- { "<leader>xs", "<cmd>XcodebuildFailingSnapshots<cr>", desc = "Show Failing Snapshots" },
+
+        -- { "<leader>xp", "<cmd>XcodebuildPreviewGenerateAndShow<cr>", desc = "Generate Preview" },
+        -- { "<leader>x<cr>", "<cmd>XcodebuildPreviewToggle<cr>", desc = "Toggle Preview" },
+
+        { "<leader>xd", "<cmd>XcodebuildSelectDevice<cr>", desc = "Select Device" },
+        -- { "<leader>xq", "<cmd>FzfLua quickfix<cr>", desc = "Show QuickFix List" },
+
+        -- { "<leader>xx", "<cmd>XcodebuildQuickfixLine<cr>", desc = "Quickfix Line" },
+        -- { "<leader>xa", "<cmd>XcodebuildCodeActions<cr>", desc = "Show Code Actions" },
     },
     cmd = {
         -- General
@@ -108,7 +129,7 @@ return {
     init = function()
         local wk = require("which-key")
         wk.add({
-            { "<leader>x", group = "Xcode Build" },
+            { "<leader>x", group = "Xcode" },
         })
     end,
     config = function()

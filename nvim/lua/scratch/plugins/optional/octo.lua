5544fa1f3f140c21f4655539502d4fb23dd60bad
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-11-10 02:29:37 +0100

    feat(octo): set fzf-lua as default picker
    Configured octo.nvim to use "fzf-lua" as the default picker for actions,
    improving the selection interface. Removed commented and unused options
    for clarity.

--- nvim/lua/scratch/plugins/optional/octo.lua
+++ nvim/lua/scratch/plugins/optional/octo.lua
@@ -13,11 +13,8 @@ return {
     config = function()
         require("octo").setup({
             enable_builtin = true, -- shows a list of builtin actions when no action is provided
-            default_merge_method = "commit", -- default merge method which should be used for both `Octo pr merge` and merging from picker, could be `commit`, `rebase` or `squash`
-            default_delete_branch = false, -- whether to delete branch when merging pull request with either `Octo pr merge` or from picker (can be overridden with `delete`/`nodelete` argument to `Octo pr merge`)
 
-            -- when unset, the default vim.ui.select is used.
-            -- picker = "fzf-lua", -- or "telescope"
+            picker = "fzf-lua", -- or "telescope", "fzf-lua" or "snacks"
         })
     end,
 }

94281c2d399437fd28c323ec8675ef7da5bfec8a
Andrey A. Ugolnik <andrey@ugolnik.info>
2026-01-22 08:56:01 +0100

    feat(dap-ui): add custom 'Restart/Stop' button to dap-view winbar
    Adds a 'term_restart' custom button to the dap-view winbar controls.
    This button allows users to restart or stop the debug session with
    different click actions. Also updates the dap-view setup to use the
    new controls and removes the old opts table.

--- nvim/lua/scratch/plugins/dap/dap-ui.lua
+++ nvim/lua/scratch/plugins/dap/dap-ui.lua
@@ -28,11 +28,52 @@ return {
             { "<leader>du", "<cmd>DapViewToggle<cr>", desc = "Dap UI" },
         },
         config = function()
+            require("dap-view").setup({
+                switchbuf = "uselast,useopen",
+                winbar = {
+                    controls = {
+                        enabled = true,
+                        buttons = {
+                            "play",
+                            "step_into",
+                            "step_over",
+                            "step_out",
+                            "term_restart",
+                        },
+                        custom_buttons = {
+                            -- Stop | Restart
+                            -- Double click, middle click or click with a modifier disconnect instead of stopping
+                            term_restart = {
+                                render = function(session)
+                                    local group = session and "ControlTerminate" or "ControlRunLast"
+                                    local icon = session and "" or ""
+                                    return "%#NvimDapView" .. group .. "#" .. icon .. "%*"
+                                end,
+                                action = function(clicks, button, modifiers)
+                                    local dap = require("dap")
+                                    local alt = clicks > 1
+                                        or button ~= "l"
+                                        or modifiers:gsub(" ", "") ~= ""
+                                    if not dap.session() then
+                                        dap.run_last()
+                                    elseif alt then
+                                        dap.disconnect()
+                                    else
+                                        dap.terminate()
+                                    end
+                                end,
+                            },
+                        },
+                    },
+                },
+            })
+
             vim.api.nvim_create_autocmd({ "FileType" }, {
                 pattern = {
                     "dap-view",
                     "dap-view-term",
                     "dap-repl", -- dap-repl is set by `nvim-dap`
+                    "term_restart",
                 },
                 callback = function(args)
                     vim.opt_local.cc = ""
@@ -42,14 +83,6 @@ return {
                 end,
             })
         end,
-        opts = {
-            switchbuf = "uselast,useopen",
-            winbar = {
-                controls = {
-                    enabled = true,
-                },
-            },
-        },
     },
 
     --[[

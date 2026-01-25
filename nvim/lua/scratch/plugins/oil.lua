cb24678ef0785585c75fc46e85509e58d64286b5
Andrey A. Ugolnik <andrey@ugolnik.info>
2026-01-25 14:44:16 +0100

    style(oil): simplify winbar return logic for current dir fallback
    Refactors conditional to streamline returning buffer name when current
    directory is unavailable.

--- nvim/lua/scratch/plugins/oil.lua
+++ nvim/lua/scratch/plugins/oil.lua
@@ -6,10 +6,9 @@ function _G.get_oil_winbar()
     local dir = require("oil").get_current_dir(bufnr)
     if dir then
         return vim.fn.fnamemodify(dir, ":~")
-    else
-        -- If there is no current directory (e.g. over ssh), just show the buffer name
-        return vim.api.nvim_buf_get_name(0)
     end
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
 end
 
 return {

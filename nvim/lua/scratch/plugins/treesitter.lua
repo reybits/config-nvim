c05d4a6a22d95d1da83827a8060970f4233920fa
Andrey A. Ugolnik <andrey@ugolnik.info>
2026-04-08 21:55:38 +0200

    feat(treesitter): replace ts-install and nvim-treesitter with ts-forge
    - Switch to using reybits/ts-forge.nvim for Treesitter parser management.
    - Removes ts-install.nvim and disables direct use of nvim-treesitter.
    - Enables auto_install and updates ensure_installed list for ts-forge.

--- nvim/lua/scratch/plugins/treesitter.lua
+++ nvim/lua/scratch/plugins/treesitter.lua
@@ -1,54 +1,32 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        branch = "main",
-        lazy = false,
-        init = function()
-            -- Disable the buggy plugin code; only use query files
-            -- (highlights, injections, folds, etc.)
-            vim.g.loaded_nvim_treesitter = 1
-        end,
-    },
-
-    {
-        "lewis6991/ts-install.nvim",
-        dependencies = { "nvim-treesitter/nvim-treesitter" },
-        lazy = false,
-        config = function()
-            require("ts-install").setup({
-                ensure_install = {
-                    "bash",
-                    "c",
-                    "cpp",
-                    "cmake",
-                    "make",
-                    "css",
-                    "html",
-                    "javascript",
-                    "java",
-                    "json",
-                    "lua",
-                    "markdown",
-                    "markdown_inline",
-                    "python",
-                    "query",
-                    "regex",
-                    "tsx",
-                    "typescript",
-                    "gitignore",
-                    "vim",
-                    "vimdoc",
-                    "yaml",
-                },
-                auto_install = false,
-            })
-
-            vim.api.nvim_create_autocmd("FileType", {
-                group = vim.api.nvim_create_augroup("scratch_treesitter", { clear = true }),
-                callback = function(args)
-                    pcall(vim.treesitter.start, args.buf)
-                end,
-            })
-        end,
-    },
+    "reybits/ts-forge.nvim",
+    config = function()
+        require("ts-forge").setup({
+            auto_install = true,
+            ensure_installed = {
+                "bash",
+                "c",
+                "cpp",
+                "cmake",
+                "make",
+                "css",
+                "html",
+                "javascript",
+                "java",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "tsx",
+                "typescript",
+                "gitignore",
+                "vim",
+                "vimdoc",
+                "yaml",
+            },
+        })
+    end,
 }

448d674f63ff5f451009b59ba6add2b1fb08e623
Andrey A. Ugolnik <andrey@ugolnik.info>
2026-04-08 21:12:51 +0200

    feat(treesitter): switch to ts-install.nvim for parser management
    Replace direct nvim-treesitter setup with ts-install.nvim to handle
    parser installation. Disable nvim-treesitter's plugin code, using only
    query files for highlights and folds. Simplify autocmd to start
    treesitter highlighting per buffer.
    
    BREAKING CHANGE: nvim-treesitter plugin code is disabled; parser
    management is now handled by ts-install.nvim. Custom folding and large
    file logic removed. Manual intervention may be needed for advanced
    treesitter features.

--- nvim/lua/scratch/plugins/treesitter.lua
+++ nvim/lua/scratch/plugins/treesitter.lua
@@ -1,75 +1,54 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    -- API has changed, and not supported for lazy loading anymore.
-    -- So, disable lazy loading here and commented out the event and cmd fields.
-    lazy = false,
-    -- event = {
-    --     "BufReadPost",
-    --     "BufNewFile",
-    -- },
-    -- cmd = {
-    --     "TSUpdateSync",
-    --     "TSUpdate",
-    --     "TSInstall",
-    -- },
-    config = function()
-        local treesitter = require("nvim-treesitter")
-        treesitter.setup({
-            install_dir = vim.fn.stdpath("data") .. "/site",
-        })
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        lazy = false,
+        init = function()
+            -- Disable the buggy plugin code; only use query files
+            -- (highlights, injections, folds, etc.)
+            vim.g.loaded_nvim_treesitter = 1
+        end,
+    },
 
-        treesitter.install({
-            "bash",
-            "c",
-            "cpp",
-            "cmake",
-            "make",
-            "css",
-            "html",
-            "javascript",
-            "java",
-            "json",
-            "lua",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "query",
-            "regex",
-            "tsx",
-            "typescript",
-            "gitignore",
-            "vim",
-            "vimdoc",
-            "yaml",
-        })
+    {
+        "lewis6991/ts-install.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        lazy = false,
+        config = function()
+            require("ts-install").setup({
+                ensure_install = {
+                    "bash",
+                    "c",
+                    "cpp",
+                    "cmake",
+                    "make",
+                    "css",
+                    "html",
+                    "javascript",
+                    "java",
+                    "json",
+                    "lua",
+                    "markdown",
+                    "markdown_inline",
+                    "python",
+                    "query",
+                    "regex",
+                    "tsx",
+                    "typescript",
+                    "gitignore",
+                    "vim",
+                    "vimdoc",
+                    "yaml",
+                },
+                auto_install = false,
+            })
 
-        vim.api.nvim_create_autocmd("FileType", {
-            group = vim.api.nvim_create_augroup("scratch_treesitter", { clear = true }),
-            pattern = "*",
-            callback = function(args)
-                local buf = args.buf
-
-                -- Enable folding
-                -- vim.wo[0][0].foldmethod = "expr"
-                -- vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-
-                -- Enable highlighting
-                pcall(vim.treesitter.start, buf)
-
-                -- Disable for large files
-                local max_size = 1024 * 100
-                local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_size then
-                    vim.treesitter.stop(buf)
-                    return
-                end
-
-                local max_lines = 5000
-                if vim.api.nvim_buf_line_count(buf) > max_lines then
-                    vim.treesitter.stop(buf)
-                end
-            end,
-        })
-    end,
+            vim.api.nvim_create_autocmd("FileType", {
+                group = vim.api.nvim_create_augroup("scratch_treesitter", { clear = true }),
+                callback = function(args)
+                    pcall(vim.treesitter.start, args.buf)
+                end,
+            })
+        end,
+    },
 }
